eac1b506a411f5720927abaf7822aab513b9297a
Roland Zeng <rzeng@g.ucla.edu>
2015-11-14 01:38:11 -0800

    final product

---
+++ README.md
@@ -0,0 +1,43 @@
+Blizzard Take-Home Test (World of Warcraft API)
+
+Live Website:
+http://blizzardAPI.herokuapp.com
+
+Summary:
+I used NodeJS with MongoDB to simulate a World of Warcraft API that stores account and character data. I only implemented the technical specs (exposing each API command). I did not implement the rules (race-class compatibility, level bounding, etc.) since those would have taken much longer. It is for this same reason that I have not implemented any capitalization-checking - Please make sure your API calls use the same lower/uppercase when setting/searching for names.
+
+The full spec is attached under Blizzard_Automation_API_Spec.txt
+
+Feel free to try them youself! The database is open to anyone- just use Postman REST client. When making POST requests, make sure the input is sent as "x-www-form-urlencoded."
+
+The example commands from the spec are pasted below.
+
+•         GET {your-service-url}/about
+                 returns 200 with body of:
+                 { "author" : "Jon Doe Applicant", "source" : "/relative/path/to/source/code.language" }
+
+•         POST {your-service-url}/account
+                  { "name" : "testaccount" }
+                  returns 200 with body of:
+                  { "account_id" : 1234 }
+
+
+•         GET {your-service-url}/account
+                  returns 200 with body of (all accounts):
+                  { "accounts" : [ { "account_id" : 1234, "account_name" : "blah", "link" : "{your-service-url}/account/blah" }, ... ] }
+
+•         POST {your-service-url}/account/{account_name}/characters
+                  { "name" : "Lochtar", "race" : "Orc", "class" : "Druid", "faction" : "Alliance", "level" : 90 }
+                  returns 200 with body of:
+                  { "character_id" : 1234 }
+
+
+•         DELETE {your-service-url}/account/{account_name}
+                  returns 200
+
+•         DELETE {your-service-url}/account/{account_name}/characters/{character_name}
+                  returns 200
+
+•         GET {your-service-url}/account/{account_name}/characters
+                  returns 200 with body of:
+                  { "account_id" : 1234, "characters" : [ { "character_id" : 1234, "name" : "Lochtar", "race" : "Orc", "class" : "Druid", "faction" : "Alliance", "level" : 90 } ] }
--- server.js
+++ server.js
@@ -17,21 +17,23 @@ db.on('error', console.error.bind(console, 'connection error:'));
 //DATABASE SCHEMA SETUP
 var AccountSchema = mongoose.Schema({
   account_name: String,
-  account_id: Number,
-  characters: {
-      character:{
-        character_id: Number,
-        name: String,
-        race: String,
-        class: String,
-        faction: String,
-        level: Number,
-      }
-  }
+  account_id: String,
+  link: String
+})
 
-});
+var CharacterSchema = mongoose.Schema({
+  account_id: String,
+  character_id: String,
+  name: String,
+  race: String,
+  class: String,
+  faction: String,
+  level: Number
+
+})
 
 var Account = mongoose.model('Account', AccountSchema);
+var Character = mongoose.model('Character', CharacterSchema);
 
 
 var router = express.Router();
@@ -41,18 +43,18 @@ router.get('/', function(req, res) {
 });
 
 router.get('/about', function(req, res) {
-    res.status(200).json({ "author" : "Roland Zeng", "source" : "tba" });
+    res.status(200).json({ "author" : "Roland Zeng", "source" : "https://github.com/rzeng95/blizzardAPI" });
 });
 
-//GET localhost:5000/account should list all accounts 
-//POST localhost:5000/account with params 'name' and 'bob' should add bob as an account and return bob's new account ID 
+//GET localhost:5000/account should list all accounts
+//POST localhost:5000/account with params 'name' and 'bob' should add bob as an account and return bob's new account ID
 router.route('/account')
   .get(function(req,res){
-    Account.find(function(err, accounts) {
+    Account.find(function(err, accs) {
 			if (err)
 				res.status(500).send(err);
       else {
-        res.status(200).json(accounts);
+        res.status(200).json({accounts: accs});
       }
 
 		});
@@ -62,11 +64,14 @@ router.route('/account')
     var query = Account.find({'account_name': req.body.name}, function(err,match) {
       if (match.length === 0) {
         acc.account_name = req.body.name;
+        acc.account_id = (acc._id).toString()
+        acc.link = 'http://blizzardAPI.herokuapp.com/account/' + req.body.name
         acc.save(function(err) {
           if (err)
             res.status(500).send(err);
           else {
-            res.status(200).json({ account_id: acc._id });
+
+            res.status(200).json({ account_id: acc.account_id });
           }
         });
       }
@@ -77,7 +82,8 @@ router.route('/account')
     });
   });
 
-//GET localhost:5000/account/bob should return all of bob's characters
+//GET localhost:5000/account/bob isn't one of the requirements, but I added it in to link the user to the /characters page
+//DELETE localhost:5000/account/bob deletes bob account
 router.route('/account/:acc_name')
   .get(function(req,res){
     Account.find({'account_name': req.params.acc_name}, function(err,match){
@@ -85,19 +91,116 @@ router.route('/account/:acc_name')
         res.status(404).json({error: 'couldn\'t find account'})
       }
       else {
-        res.status(200).json({message: 'found! - todo'})
+        var url = 'http://blizzardAPI.herokuapp.com/account/' + req.params.acc_name + '/characters'
+        var msg = 'Found account! Go to ' + url +' to view full character list'
+        res.status(200).json({message: msg})
       }
 
-    });
+    })
 
-  });
+  })
+
+  .delete(function(req,res) {
+    Account.remove({'account_name': req.params.acc_name}, function(err,result){
+      if (err)
+        res.send(err);
+      else {
+        res.status(200).json({message: 'account deleted!'})
+      }
+    })
+
+  })
+
+//GET localhost:5000/account/bob/characters should return all of bob's characters
+//POST localhost:5000/account/bob/characters with the character specs should add a new character
+router.route('/account/:acc_name/characters')
+  .get(function(req,res){
+
+    var query1 = Account.find({'account_name': req.params.acc_name}, function(err,match) {
+      if (match.length === 0) {
+        res.status(404).json({error: 'couldn\'t find account'})
+      }
+      else {
+        queried_account_id = match[0].account_id
+        var query2 = Character.find({'account_id' : queried_account_id}, function(err,match2){
+          var charMap = {};
+
+          var i = 0;
+          while (match2[i] !== undefined) {
+            charMap[match2[i].name] = match2[i]
+            i++;
+          }
+
+          res.status(200).json({account_id: queried_account_id, characters: charMap})
+        })
+      }
+    })
+  })
+
+  .post(function(req,res){
+
+    var query1 = Account.find({'account_name': req.params.acc_name}, function(err,match) {
+      if (match.length === 0) {
+        res.status(404).json({error: 'couldn\'t find account'})
+      }
+      else {
+        var character = new Character();
+        character.character_id = (character._id).toString()
+        var query2 = Character.find({'name': req.body.name}, function(err,match2) {
+          if (match2.length === 0) {
+
+            character.account_id = match[0].account_id;
+            character.name = req.body.name;
+            character.race = req.body.race;
+            character.class = req.body.class;
+            character.faction = req.body.faction;
+            character.level = req.body.level;
+            character.save(function(err) {
+              if (err)
+                res.status(500).send(err);
+              else {
+                res.status(200).json({ character_id: character._id });
+              }
+            });
+
+          }
+          else {
+            res.status(422).json({error: 'character name already taken!'})
+          }
+
+        })
+      }
+    })
+  })
+
+//DELETE localhost:5000/account/bob/characters/JOE should delete JOE from list of characters
+router.route('/account/:acc_name/characters/:character_name')
+  .delete(function(req, res) {
+      var query1 = Account.find({'account_name': req.params.acc_name}, function(err,match) {
+      if (match.length === 0) {
+        res.status(404).json({error: 'couldn\'t find account'})
+      }
+      else {
+        var query2 = Character.find({'name': req.params.character_name}, function(err,match2){
+          if(match2.length === 0) {
+            res.status(404).json({error: 'couldn\'t find character to delete'})
+          }
+          else {
+            Character.remove({'name' : req.params.character_name}, function(err, result) {
+              if (err) res.send(err)
+              else res.status(200).json({message: 'Successfully deleted account!'})
+            })
+          }
+
+        })
+
+      }
+    })
+
+
+})
 
 app.use('/', router);
-
-
-
-
-
 
 app.listen(app.get('port'), function() {
   console.log('App is running on port', app.get('port'));
